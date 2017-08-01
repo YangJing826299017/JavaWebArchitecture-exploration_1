d746f655ed6063f9badb7a023e586551b61f8505
YangJing826299017 <826299017@qq.com>
2017-08-02 01:21:29 +0800

    Commit

--- src/main/java/org/smart4j/chapter1/HelloServlet.java
+++ src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -34,8 +34,6 @@ public class HelloServlet extends HttpServlet {
 		DateFormat format=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		String currentTime=format.format(new Date());
 		request.setAttribute("currentTime",currentTime);
-		System.out.println("HelloWorld");
-		System.out.println("HelloWorld");
 		request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request,response);
 	}
 
